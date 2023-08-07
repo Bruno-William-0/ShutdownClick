9bbb88cecc7703e54ed97284abe24d740946a1aa
Bruno-William-0 <bruwill04@gmail.com>
2023-08-06 23:47:23 -0300

    Shutdown

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 import Commands from './Controller/Commands.js'
 
 const app = express();
+
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+  }));
+  /*app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permite solicitações de qualquer origem
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });*/
 
 app.use('/shutdown', Commands.shutdown)
 
